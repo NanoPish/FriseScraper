88acae1e9c549c36520cc424f2bae5e87daae9d4
François Gayraud <francois.gayraud@epitech.eu>
2018-03-21 18:14:05 +0100

    pandascore Dota 2

---
+++ sources/main.py
@@ -0,0 +1,27 @@
+from wikipedia import vizgr_api_wrapper
+from pandascore import pandascore_dota_api
+from historical_event import historical_event
+from mysql_database import mysql_database
+
+if __name__ == '__main__':
+    #example querying all events between 1990 and 1992 from wikipedia using vizgr api
+    wikipedia = vizgr_api_wrapper()
+    pandascore = pandascore_dota_api()
+    pandascore.make_query()
+    pandascore_events = pandascore.get_event_list()
+    wikipedia.make_query(begin_date='19900000', end_date='19900200')
+    wikipedia_events = wikipedia.get_event_list()
+    database = mysql_database()
+    for event in wikipedia_events:
+        print('Trying to insert event: \n', event, '\n')
+        insert_event_query = ('INSERT INTO events '
+                              '(date, description, source) '
+                              'VALUES (%s, %s, %s)')
+        database.query(insert_event_query, (event.date, event.description, event.source))
+    for event in pandascore_events:
+        print('Trying to insert event: \n', event, '\n')
+        insert_event_query = ('INSERT INTO events '
+                              '(date, description, source) '
+                              'VALUES (%s, %s, %s)')
+        database.query(insert_event_query, (event.date, event.description, event.source))
+    database.cnx.commit()
--- sources/pandascore.py
+++ sources/pandascore.py
@@ -1,20 +1,34 @@
 import requests
-from xml.etree import ElementTree
+import json
 
-class pandascore_api:
-    url_endpoint = '/teams'
+import datetime
+import dateutil.parser
+from collections import namedtuple
+from historical_event import historical_event
+
+class pandascore_dota_api:
+    url_endpoint = 'https://api.pandascore.co/videogames/4/tournaments/'
+    url_team = 'https://api.pandascore.co/teams'
     source_name = 'panda_score'
+    api_result = []
 
     def __init__(self):
         self.query_result = None
 
     def make_query(self):
         query_dict = {
-            'access_token':'lvyy4BTBrK_e3Xt2WLxi-KpFYq448BwPrRyweHHm2cYg_BDYqGE'
+            'token':'JBsdKIMFwx962sNhH_tqrAzRDKn_AK82_UXbYKYeDP2v9lMRXXE'
         }
+        self.query_result = requests.get(pandascore_dota_api.url_endpoint, params=query_dict)
+        tournaments = json.loads(self.query_result.content, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+        for tournament in tournaments:
+            if tournament.winner_id != None:
+                self.url_team = 'https://api.pandascore.co/teams/' + str(tournament.winner_id)
+                self.query_result = requests.get(self.url_team, params=query_dict)
+                team = json.loads(self.query_result.content, object_hook=lambda d: namedtuple('X', d.keys())(*d.values()))
+                # print("TOURNAMENT : " + tournament.slug + " " + tournament.name  + " WIN BY : " + team.name + " AT " + tournament.begin_at)
+                self.api_result.append(historical_event(description="TOURNAMENT " + tournament.slug + " " + tournament.name + " WIN BY " + tournament.name, date=dateutil.parser.parse(tournament.begin_at).date(), source=self.source_name, name="DOTA 2"))
         
-        self.query_result = requests.get(pandascore_api.url_endpoint, params=query_dict)
-        print(self.query_result)
-
     def get_event_list(self):
-        event_list = []
+        return(self.api_result)
+            
